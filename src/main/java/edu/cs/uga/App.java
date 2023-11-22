20396e2cc261dea6a9d259b04ee41358ef445f9e
kamal <lamakkm@gmail.com>
2023-11-22 15:04:28 -0500

    initial commit

---
+++ src/main/java/edu/cs/uga/App.java
@@ -0,0 +1,16 @@
+package edu.cs.uga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The main springboot application class.
+ */
+@SpringBootApplication
+public class App
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
