f7c19db6773fd468a0123014396f05e2d8f0e4e6
kamal <lamakkm@gmail.com>
2023-11-22 15:55:58 -0500

    remove lombok dependency

--- src/main/java/edu/cs/uga/IndexController.java
+++ src/main/java/edu/cs/uga/IndexController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * Controller for index.html
+ */
 @Controller
 public class IndexController {
     @GetMapping("")
